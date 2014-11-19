f5cd4ead6fce6de8901229e9b7218dc3b8764bdf
daften <dieterblomme@gmail.com>
2014-11-19 01:24:11 +0100

    Easier install for ansible

--- install.sh
+++ install.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 
 function ansible_run() {
+  sudo easy_install pip;
+  sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible;
+
   DIR=${PWD}
-  echo ${DIR}
-  TMP_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
-
-  cd ${TMP_DIR}
-  git clone http://github.com/ansible/ansible.git ${TMP_DIR}/ansible
-
-  export PATH=${TMP_DIR}/ansible/bin:${PATH}
-  export ANSIBLE_LIBRARY=${TMP_DIR}/ansible/library
-  export PYTHONPATH=${TMP_DIR}/ansible/lib:${PYTHONPATH}
-
-  cd ${DIR}
-  echo $(`which ansible-playbook`)
   ansible-playbook -i ${DIR}/inventory -vvvv ${DIR}/mac.yml
-
-  rm -rf ${TMP_DIR}
 }
 
 ansible_run
